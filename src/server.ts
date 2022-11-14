b3286287976c1588c9c50bb5249fafac57b8a75b
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2022-11-14 11:39:41 -0300

    🦄 refactor: alterada o filtro para count

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 
 // PRIMEIRA FUNÇÃO QUE SERÁ EXECUTADA NO PROJETO. O FASTIFY É O QUE VAI HOSPEDAR A APLICAÇÃO E O LOGGER SÃO OS LOGS
 async function bootstrap() {
@@ -7,8 +12,9 @@ async function bootstrap() {
   });
 
   // PRIMEIRA ROTA DA APLICAÇÃO = CONTADOR DE BOLÃO
-  fastify.get("/pools/count", () => {
-    return { count: 0 };
+  fastify.get("/pools/count", async () => {
+    const count = await prisma.pool.count();
+    return { count };
   });
 
   await fastify.listen({ port: 3333 });
