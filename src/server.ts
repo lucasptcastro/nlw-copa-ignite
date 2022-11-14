3fef2d3df2c161a67218d3e33301f515b2cddacf
lucasptcastro@gmail.com <lucasptcastro@gmail.com>
2022-11-14 11:49:53 -0300

    ✨ feat: adicionada permissão de consumo do backend

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
@@ -11,13 +12,18 @@ async function bootstrap() {
     logger: true,
   });
 
+  // PERMITE QUE QUALQUER APLICAÇÃO CONSUMA O BACKEND
+  await fastify.register(cors, {
+    origin: true,
+  });
+
   // PRIMEIRA ROTA DA APLICAÇÃO = CONTADOR DE BOLÃO
   fastify.get("/pools/count", async () => {
     const count = await prisma.pool.count();
     return { count };
   });
 
-  await fastify.listen({ port: 3333 });
+  await fastify.listen({ port: 3333, host: "0.0.0.0" });
 }
 
 bootstrap();
